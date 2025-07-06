214709d3c64331b89a1ec6d0e76ee804d162c2d3
tigocode <tiago.dantas16@live.com>
2025-07-06 16:39:31 -0300

    refactor: remover geração de número aleatório e ajustar lógica de criação de instâncias

--- src/services/instanceGenerator.js
+++ src/services/instanceGenerator.js
@@ -1,12 +1,16 @@
-const instanceGenerator = async (company, name) => {
-  const hashNumber = Math.floor(Math.random() * 900000) + 100000;
+const crypto = require('crypto');
 
+const instanceGenerator = async (company, name) => {
   const hashcompany = await extractedFirstAndLast(company);
   const hashname = await extractedFirstAndLast(name);
 
-  const instance = `${hashcompany}${hashname}-${hashNumber}`;
+  const instance = `${hashcompany}${hashname}`;
 
-  return instance;
+  const hash = crypto.createHash('sha256').update(instance).digest('hex');
+  const numericHash = parseInt(hash.slice(0, 8), 16);
+  const code = (numericHash % 900000) + 100000;
+  
+  return code;
 };
 
 const extractedFirstAndLast = async (text) => {
