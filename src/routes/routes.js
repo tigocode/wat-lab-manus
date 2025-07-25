b361a48e278ad02a77a85f58dd959d67b61d63ca
tigocode <tiago.dantas16@live.com>
2025-07-24 23:10:29 -0300

    feat: adicionar mensagem de sucesso na rota raiz da API

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const instance = require('../controllers/createInstances.controller');
 
+router.get('/', (req, res) => {
+  res.status(200).json({ message: '✅ API WatAPI rodando com sucesso!' });
+});
+
 router.post('/start-session', instance.startSession);
 
 module.exports = router;
